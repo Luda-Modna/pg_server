63e683dd6c7b6c4fcecf77908652ff134a420555
Luda Modna <ludmilamodna3@gmail.com>
2025-09-12 16:16:18 +0300

    Add class User

--- index.js
+++ index.js
@@ -67,29 +67,65 @@ const ln = 'Petrenko1';
 
 (async () => {
   try {
-    const user = await pool.query(`
+    const user = await pool.query(
+      `
       SELECT *
       FROM users
       WHERE first_name = $1 AND last_name = $2
-      `, [fn, ln]);
-      console.log('user=>',user.rows[0])
+      `,
+      [fn, ln]
+    );
+    console.log('user=>', user.rows[0]);
   } catch (err) {
     console.log(err);
   }
 })();
 
-const user_id= 1;
+const user_id = 1;
 const created_at = '2023-10-10';
 
 (async () => {
   try {
-    const order = await pool.query(`
+    const order = await pool.query(
+      `
      INSERT INTO orders (user_id, created_at)
      VALUES ($1, $2)
      RETURNING *
-      `, [user_id, created_at]);
-      console.log('order => ',order.rows[0])
+      `,
+      [user_id, created_at]
+    );
+    console.log('order => ', order.rows[0]);
   } catch (err) {
     console.log(err);
   }
 })();
+
+class User {
+  static async create ({ firstName, lastName, email, tel }) {
+    try {
+      const insertQuery = `
+       INSERT INTO users (first_name, last_name, email, tel)
+       VALUES ('${firstName}', '${lastName}', '${email}', '${tel}')
+       RETURNING *
+        `;
+      const crreatedUser = await pool.query(insertQuery);
+      return crreatedUser.rows[0];
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static getAll ({ limit, offset }) {}
+  static getById (id) {}
+  static updateById (id, { firstName, lastName, email, tel }) {}
+  static deleteById (id) {}
+}
+
+const newUser = {
+  firstName: 'Miki',
+  lastName: 'Mik',
+  email: 'mail@mail.com',
+  tel: '+380123456789',
+};
+
+const createdUser = User.create(newUser).then(data => console.log(data));
